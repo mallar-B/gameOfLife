869748ff7ab1ee78d87c05e1bcffea61dc913a10
mallar-B <bhattacharyamallar@gmail.com>
2025-06-25 20:11:08 +0530

    draw in a grid in canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [gridSize, setGridSize] = useState<number>(20);
+  const [frameWidth, setFrameWidth] = useState<number>(0);
+  const [frameHeight, setFrameHeight] = useState<number>(0);
+  const [rows, setRows] = useState<number>(0);
+  const [cols, setCols] = useState<number>(0);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const isDrawing = useRef(false);
+
+
+  const draw = (event: MouseEvent) => {
+    if (!isDrawing.current) return;
+
+    const ctx = canvasRef.current?.getContext("2d");
+    const frame = canvasRef.current?.getBoundingClientRect();
+    const x = Math.floor((event.clientX - frame!.left) / gridSize) * gridSize;
+    const y = Math.floor((event.clientY - frame!.top) / gridSize) * gridSize;
+
+    ctx?.fillRect(x, y, gridSize, gridSize);
+  };
+
+  useEffect(() => {
+    setFrameWidth(window.innerWidth - 50); // compansate for padding of body
+    setFrameHeight(window.innerHeight / 2);
+  }, []);
+
+  useEffect(() => {
+    canvasRef.current?.addEventListener(
+      "mouseleave",
+      () => (isDrawing.current = false),
+    );
+    canvasRef.current?.addEventListener(
+      "mouseup",
+      () => (isDrawing.current = false),
+    );
+
+    canvasRef.current?.addEventListener("mousedown", (event) => {
+      isDrawing.current = true;
+      draw(event);
+    });
+    canvasRef.current?.addEventListener("mousemove", (event) => draw(event));
+
+    return () => {
+      canvasRef.current?.removeEventListener(
+        "mouseleave",
+        () => (isDrawing.current = false),
+      );
+      canvasRef.current?.removeEventListener(
+        "mouseup",
+        () => (isDrawing.current = false),
+      );
+      canvasRef.current?.removeEventListener("mousedown", (event) => {
+        isDrawing.current = true;
+        draw(event);
+      });
+      canvasRef.current?.removeEventListener("mousemove", (event) =>
+        draw(event),
+      );
+    };
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="grid-frame">
+      <canvas ref={canvasRef} height={frameHeight} width={frameWidth} />
+    </div>
+  );
 }
 
-export default App
+export default App;
